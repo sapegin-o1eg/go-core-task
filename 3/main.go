35f742a2b64441f7e0a77f8dcc7f78f3e85fb974
Oleg Sapegin <sapegin.o@gmail.com>
2025-08-07 17:55:42 +0300

    Task3 is done

---
+++ 3/main.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+type StringIntMap struct {
+	m map[string]int
+}
+
+func NewStringIntMap() *StringIntMap {
+	return &StringIntMap{
+		m: make(map[string]int),
+	}
+}
+
+func (sm *StringIntMap) Add(key string, value int) {
+	sm.m[key] = value
+}
+
+func (sm *StringIntMap) Delete(key string) {
+	delete(sm.m, key)
+}
+
+func (sm *StringIntMap) Copy() map[string]int {
+	copy := make(map[string]int, len(sm.m))
+	for key, value := range sm.m {
+		copy[key] = value
+	}
+	return copy
+}
+
+func (sm *StringIntMap) Get(key string) (value int, ok bool) {
+	value, ok = sm.m[key]
+	return
+}
+
+func (sm *StringIntMap) Exists(key string) bool {
+	_, ok := sm.m[key]
+	return ok
+}
+
+func main() {
+	sm1 := NewStringIntMap()
+
+	// Different output formats:
+	fmt.Printf("[sm1] Standard output: %v\n", sm1)
+	fmt.Printf("[sm1] Without pointer (dereference): %v\n", *sm1)
+	fmt.Printf("[sm1] Only map content: %v\n", sm1.m)
+	fmt.Printf("[sm1] Pretty output: %+v\n", sm1)
+	fmt.Printf("[sm1] Detailed output: %#v\n", sm1)
+
+	sm1.Add("a", 1)
+	fmt.Printf("[sm1] After adding element: %+v\n", sm1.m)
+	sm1.Add("b", 2)
+	sm1.Add("c", 3)
+	sm1.Add("d", 4)
+	sm1.Add("e", 5)
+	sm1.Add("f", 6)
+	sm1.Add("g", 7)
+	sm1.Add("h", 8)
+	sm1.Add("i", 9)
+	sm1.Add("j", 10)
+	sm1.Add("k", 11)
+	fmt.Printf("[sm1] After adding elements: %+v\n", sm1.m)
+
+	sm1.Delete("k")
+	fmt.Printf("[sm1] After deleting key k: %+v\n", sm1.m)
+
+	sm2 := sm1.Copy()
+	fmt.Printf("[sm2] Copy of sm1: %+v\n", sm2)
+
+	fmt.Printf("[sm1] Does key 'k' exist in sm1?: %v\n", sm1.Exists("k"))
+	fmt.Printf("[sm1] Does key 'a' exist in sm1?: %v\n", sm1.Exists("a"))
+
+	value1, ok1 := sm1.Get("a")
+	fmt.Printf("[sm1] Value of key 'a': %v, and it exists: %v\n", value1, ok1)
+
+	value2, ok2 := sm1.Get("k")
+	fmt.Printf("[sm1] Value of key 'k': %v, and it exists: %v\n", value2, ok2)
+}
